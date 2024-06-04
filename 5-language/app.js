11904680d22efedaadce7cf1221fff72f80ab184
Mihails Gilodo <mgilodo@gmail.com>
2024-06-04 19:54:37 +0300

    Добавил toLowerCase() к ДЗ №2

--- 5-language/app.js
+++ 5-language/app.js
@@ -1,16 +1,17 @@
 // Условие
-const userLanguage = 'es';
+const userLanguage = "en";
+const userLanguageLowerCase = userLanguage.toLowerCase();
 
-switch (userLanguage) {
-    case 'en':
-        alert('Good afternoon!');
-        break;
-    case 'de':
-        alert('Gutten tag!');
-        break;
-    case 'ru':
-        alert('Добрый день!');
-        break;
-    default:
-        alert('Sorry! Have not learned your language yet!');
+switch (userLanguageLowerCase) {
+  case "en":
+    alert("Good afternoon!");
+    break;
+  case "de":
+    alert("Gutten tag!");
+    break;
+  case "ru":
+    alert("Добрый день!");
+    break;
+  default:
+    alert("Sorry! Have not learned your language yet!");
 }
