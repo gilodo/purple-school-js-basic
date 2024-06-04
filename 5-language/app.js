9860a3404563a618b94d381ec25a4ed0d8a31cb2
Mihails Gilodo <mgilodo@gmail.com>
2024-06-04 17:58:36 +0300

    Добавлено ДЗ №2

---
+++ 5-language/app.js
@@ -0,0 +1,16 @@
+// Условие
+const userLanguage = 'es';
+
+switch (userLanguage) {
+    case 'en':
+        alert('Good afternoon!');
+        break;
+    case 'de':
+        alert('Gutten tag!');
+        break;
+    case 'ru':
+        alert('Добрый день!');
+        break;
+    default:
+        alert('Sorry! Have not learned your language yet!');
+}
